ece14c07beefdddecd3611f204dc9932538defa2
rleite-s <rleite-s@student.42sp.org.br>
2022-12-08 01:54:12 +0100

    Início de organizador

---
+++ vetor/defines.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   defines.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rleite-s < rleite-s@student.42sp.org.br    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/07 20:06:01 by rleite-s          #+#    #+#             */
+/*   Updated: 2022/12/07 20:09:10 by rleite-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DEFINES_H
+# define DEFINES_H
+
+# define PA 1
+# define SA 2
+# define RA 3
+# define RRA 4
+# define PB 5
+# define SB 6
+# define RB 7
+# define RRB 8
+# define SS 9
+# define RR 10
+# define RRR 11
+
+#endif
