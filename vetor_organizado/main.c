884555b72bb3750bb960b0f5b7c710bfcebd4f80
rleite-s <rleite-s@student.42sp.org.br>
2022-12-17 23:22:50 +0100

    Norma e remoção de .o

--- vetor_organizado/main.c
+++ vetor_organizado/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: msilva-p <msilva-p@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rleite-s < rleite-s@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/17 00:28:56 by msilva-p          #+#    #+#             */
-/*   Updated: 2022/12/17 19:57:08 by msilva-p         ###   ########.fr       */
+/*   Updated: 2022/12/17 23:19:57 by rleite-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,7 +57,7 @@ int	ft_alloc_stack(t_stack *a, t_stack *b, int args)
 	if (ft_malloc(&a->indexes, args) == 1) // malloco os espaço pros indexes
 	{
 		free(a->numbers);
-		return (1); 
+		return (1);
 	}
 	// if se deu errado
 	if (ft_malloc(&b->numbers, args) == 1)
@@ -106,18 +106,15 @@ void	ft_create_vector(int argc, char **args)
 	{
 		ft_free(&a, &b);
 		return ;
-		
 	}
-	
-	
 	ft_checker(&a, &b, a.last);
 }
 
-int ft_check_repeat_num(t_stack *a)
+int	ft_check_repeat_num(t_stack *a)
 {
-	int i;
-	int j;
-	
+	int	i;
+	int	j;
+
 	i = 0;
 	while (i < a->last)
 	{
@@ -125,7 +122,7 @@ int ft_check_repeat_num(t_stack *a)
 		while (j <= a->last)
 		{
 			if (a->indexes[j] == a->indexes[i])
-				return(1);
+				return (1);
 			j++;
 		}
 		i++;
@@ -133,7 +130,7 @@ int ft_check_repeat_num(t_stack *a)
 	return (0);
 }
 
-void ft_free(t_stack *a, t_stack *b)
+void	ft_free(t_stack *a, t_stack *b)
 {
 	free (a->numbers);
 	free (a->indexes);
@@ -145,9 +142,9 @@ void ft_free(t_stack *a, t_stack *b)
 // Função pra verificar se o usuário digitou apenas números, sem gracinhas no meio da brincadeira
 int	ft_is_only_numbers(char **arg) // lembre que argv é o que você digitou (os números estão numa matriz, mas eles ainda são lidos como strings e não como números mesmo)
 {
-	int	line;
-	int	column;
-	char *trim;
+	int		line;
+	int		column;
+	char	*trim;
 
 	line = 0;
 	while (arg[line] != NULL) // Vai percorrer até o fim da matriz
@@ -170,9 +167,6 @@ int	ft_is_only_numbers(char **arg) // lembre que argv é o que você digitou (os
 	}
 	return (0); // Se chegar aqui, o usuário digitou apenas números (zero erros)
 }
-
-//# define INT_MIN = -2147483648
-//# define INT_MAX = 2147483647
 
 long	ft_atoi_plus(const char *nptr)
 {
@@ -208,7 +202,7 @@ int	main(int argc, char **argv)
 	int	only_numbers; // Inteiro que verifica se o usuário não fez nenhuma gracinha ao invés de digitar apenas números
 
 	if (argc < 2) // Se tiver menos de 2 args, só tem o nome do arquivo
-		return(ft_error());
+		return (ft_error());
 	if (argc < 3) // Nesse caso, ele só digitou um número //verificar quando só tem 1 arg(numero)
 		return (ft_error()); // Mensagem de erro dividida em 2 comandos por conta de norma ヽ(ヅ)ノ
 	// Se passou dessa line, vamos ao menos verificar os args
